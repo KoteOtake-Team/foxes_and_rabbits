71ba79e3bf080d6ecb7f0bc9b667f7b46a55e13d
become-iron <nikitaburkov@yandex.ru>
2016-09-19 19:04:57 +0400

    наброски

---
+++ frontend/js/gameCore/eventTypes.js
@@ -0,0 +1,10 @@
+// СОБЫТИЯ
+//
+// name (String) - название
+// description (String) - описание
+// type (String) - тип
+// affect (Number/function) - воздействие
+
+eventTypes = {
+
+};
---
+++ frontend/js/gameCore/weatherTypes.js
@@ -0,0 +1,15 @@
+// ВИДЫ ПОГОДЫ
+//
+// name (String) - название
+// description (String) - описание
+// fertilityAffect (Number) - воздействие на плодородность (доли/ход)
+// animalAffect (Number) - воздействие на самочувствие животных (доли/ход)
+
+var weatherTypes = {
+    'heavyRain' : {
+        name : 'Сильный дождь',
+        description : 'Дождь ливневого характера. Характеризуются внезапностью начала и конца выпадения, резким изменением интенсивности.',
+        fertilityAffect : 1.5,
+        animalAffect : 0.75
+    }
+};
