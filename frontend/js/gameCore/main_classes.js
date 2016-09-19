e1bca896c385f9eb43359b49239a0cf635098377
become-iron <nikitaburkov@yandex.ru>
2016-09-19 18:58:46 +0400

    наброски

--- frontend/js/gameCore/main_classes.js
+++ frontend/js/gameCore/main_classes.js
@@ -1,40 +1,42 @@
-/** ТИП РЕЛЬЕФА */
-// Пример создания объекта рельефа:
-// taiga = new Relief(reliefTypes.taiga);
-function Relief(features) {
-    this.name = features.name;
-    this.texture = features.texture;
-    this.surface = features.surface;
-    this.fertility = features.fertility;  // вызывается каждый ход с параметром dayOfYear
-    this.stock_behavior = features.stock_behavior;  // вызывается каждый ход с параметром dayOfYear
-}
-
-
 /** ИГРОВАЯ КЛЕТКА */
 function Cell(relief) {
     this.relief = relief;  // объект класса Relief
-    this.unit = null;  // объект юнита на клетке
+    this.animal = [];  // объекты животных на клетке
     this.weather = null;  // погода
     this.stock = null;  // количество запасов TODO требуется базовое значение на начало игры
 
     // изменение запасов в клетке
     this.changeStock = function (dayOfYear) {
-        this.stock = this.stock + this.relief.fertility(dayOfYear) - this.relief.stock_behavior(dayOfYear);
+        // изменение плодородности в зависимости от наличия осадков
+        var fertility = this.weather != null ?
+                        this.weather.fertilityAffect * this.relief.fertility(dayOfYear) :
+                        this.relief.fertility(dayOfYear);
+        this.stock = this.stock + fertility - this.relief.stock_behavior(dayOfYear);
     }
 }
 
 
+/** СОБЫТИЕ */
+function Event(features) {
+    this.name = features.name;
+    this.description = features.description;
+    this.type = features.type;
+    this.affect = features.affect;
+}
+
+
 /** ЯДРО ИГРЫ */
-// game = new GameCore(200, 50);
+// game = new Core(200, 50);
 // game.initializeGame();
 // *действия игрока*
 // game.makeMove();
-function GameCore(width, height) {
+function Core(width, height) {
     /** ИНИЦИАЛИЗАЦИЯ ИГРЫ */
     this.initializeGame = function () {
-        // создание карты, карты погоды, расстановка юнитов -> отрисовка
+        // создание карты, карты погоды, расстановка животных -> отрисовка
     };
-    
+
+    /**  КАРТА */
     this.map = function () {
         this.width = width;
         this.height = height;
@@ -46,7 +48,7 @@ function GameCore(width, height) {
 
     /** СДЕЛАТЬ ХОД */
     this.makeMove = function () {
-        this.dayOfYear = (this.dayOfYear === 360) ? 1 : this.dayOfYear + 1;
+        this.dayOfYear = (this.dayOfYear === 360) ? 1 : this.dayOfYear + 1;  // проверка на окончание года
         this.totalDay++;
 
         // изменение запасов в клетках
--- frontend/js/gameCore/reliefTypes.js
+++ frontend/js/gameCore/reliefTypes.js
@@ -1,14 +1,16 @@
+// ТИПЫ РЕЛЬЕФА
+//
 // name - название для отображения
+// description (String) - описание
 // texture - текстура поверхности (ссылка)
-//
 // surface - тип поверхности (solid, liquid)
-//
 // fertility - плодородность (образование единиц/ход) (функция от времения года). Поступает в stock
 // stock_behavior - поведение запасов (естественная убыль единиц/ход) (функция от времения года)
 
 var reliefTypes = {
     taiga : {
         name: 'Тайга',
+        description : '',
         texture: null,
         surface: 'solid',
         fertility: function (dayOfYear) {
